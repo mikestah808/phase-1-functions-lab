745bfbece5ba8eec32d5e259a67621b45a10cdcc
Michael Stafford <michaelstafford808@gmail.com>
2021-11-29 15:29:21 -0800

    commit tree 463fba1aee24b745d684a09e8cf82c130e8a05e4

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
- function distanceFromHqInBlocks(block){
+function distanceFromHqInBlocks(block) {
     let result;
-    
-    if(block > 42){
+    if (block > 42) {
         result = block - 42;
-    }else if(block <42){
-    result = 42 - block;
-}
+    } else if (block < 42) {
+        result = 42 - block;
+    } 
     return result;
 }
 
 
-
-
-function  distanceFromHqInFeet(block){
-    distanceFromHqInBlocks(distance);
-     let distance = distanceFromHqInBlocks(block);
-
-     return distance * 264
+function distanceFromHqInFeet(feet) {
+    // distanceFromHqInBlocks(distance);
+    let distance = distanceFromHqInBlocks(feet)
+    return distance * 264;
 }
 
 
 
-function distanceTravelledInFeet(a,b){
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
     let result;
-    if(a > b){
-        result = (a-b) * 264;
-    }else if(a < b){
-        result = (b - a) * 264;
+    if (start > destination) {
+        result = (start - destination) * 264;
+    } else if (start < destination) {
+        result = (destination - start) * 264;
     }
     return result;
-}
+  }
 
-function calculatesFarePrice(start, destination){
 
-    let a = distanceTravelledInFeet(start,destination)
-        
-    if(a < 400){
-        return 0;
-    }else if (a >= 400 && a <= 2000){
-        return 2.56;
-    }else if (a >= 2000 && a <= 2500){
-        return 25
-    }else if (a > 2500){
+
+  function calculatesFarePrice(start, destination) {
+
+      let a = distanceTravelledInFeet(start, destination)
+
+      if (a < 400) {
+          return 0;
+      } else if (a >= 400 && a <= 2000) {
+          return 2.56;
+      } else if (a >= 2000 && a <= 2500) {
+          return 25;
+      } else {
         return 'cannot travel that far';
-    }
-    }
-
-
+      }
+  }
+  
